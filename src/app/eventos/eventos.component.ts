8f412c8b06cf814f61837d7eaeb24d1d8b207859
bilhares <felipe.bilhares@hotmail.com>
2020-07-02 09:22:42 -0400

    filtro

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -8,21 +8,45 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any;
 
+  eventos: any = [];
+  eventosFiltrados: any = [];
+  imagemLargura: number = 50;
+  imagemMargem: number = 2;
+  mostrarImagem: boolean = false;
+  _filtroLista: string;
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEvento(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEvento(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 
+    );
+  }
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
   }
 
-  getEventos(){
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem
+  }
+  getEventos() {
     this.http.get("https://localhost:44364/api/evento").subscribe(
       response => {
-        console.log(response);        
+        console.log(response);
         this.eventos = response;
       },
-      err => {console.log(err);
+      err => {
+        console.log(err);
       }
     );
   }
